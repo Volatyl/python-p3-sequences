52bcf25e0dafdad9ad5e76aeb82e01f332f2af2e
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-16 11:31:26 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0] if length == 1 else [0,1]
+    for i in range(2, length):
+        fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+    print([]) if length == 0 else print(fibonacci)
+
+print_fibonacci(10)
